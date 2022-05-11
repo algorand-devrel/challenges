a883d970e5927a09c6eacd39eda4d7b32deb2f75
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-05-11 11:59:58 -0400

    missed one

---
+++ js/PaymentTransaction/validate.js
@@ -0,0 +1,17 @@
+const algosdk = require("algosdk");
+const axios = require('axios')
+
+const validate_path = challengeId => `/api/challenges/${challengeId}/verify/`
+
+async function validate(challengeId, txIds) {
+
+    const result = await axios.post(verify_path(challengeId), {
+        "transaction_ids": txIds 
+    })
+
+    if(result.status !== 200) throw new Error("Validation failed: "+JSON.stringify(result.data))
+
+    return true 
+}
+
+module.exports = validate
