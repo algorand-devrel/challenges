dafba69dd773e434c6e0061352978e5d39540027
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-06-02 13:33:02 -0400

    add better todos, add incomplete asset create transaction

---
+++ js/asset_create.js
@@ -0,0 +1,76 @@
+const algosdk = require("algosdk");
+const validate = require("../validate");
+const printError = require("../error-printer");
+const { encodeAddress } = require("algosdk");
+
+const challenge_id = "TBD"
+
+const token = "";
+const server = "https://testnet-api.algonode.cloud";
+const port = 0;
+const client = new algosdk.Algodv2(token, server, port);
+const txids = [];
+
+const secretKey =
+  "sYgLa2BSnHCYG1tlugubFuoLYjGoPoHiM71JkONCn3zWYyP45GoVqoNmZ4b31uCqML4FzQ0uSEg4PXGhCNW9TA==";
+
+// Decode the secretKey into a Uint8Array from base 64
+// This will produce an array of length 64
+const secret = new Uint8Array(Buffer.from(secretKey, "base64"));
+const acct = {
+  // The public key is the secret[32:], or the last 32 bytes
+  // We encode it to the address which is easier to read and includes a checksum
+  addr: encodeAddress(secret.slice(32)),
+  // We need not do anything with the secret
+  sk: secret,
+};
+
+(async function () {
+  try {
+    // Get the suggested parameters from the Algod server. 
+    // These include current fee levels and suggested first/last rounds.
+    const sp = await client.getTransactionParams().do();
+
+    // Construct an asset create transaction. 
+    // An asset create transaction is the same as an asset config transaction
+    // with its asset id set to 0
+    const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+      // TODO: fill in the transaction parameters to create a new Asset 
+      // with total units of 100 but max supply of 1
+      from:  undefined,     // The from address should be your address
+      manager: undefined,   // The manager address should be your address
+      assetURL: undefined,  // The asset URL should be the string "https://developer.algorand.org"
+      unitName: undefined,  // The unit name should be the string "tsk"
+      assetName: undefined, // The name should be the string "Task Asset"
+      decimals: undefined,  // The decimals should be 2 
+      total: undefined,     // The total should be 100
+      suggestedParams: sp,
+    });
+
+    // Sign the transaction. This should return a 
+    // Uint8Array representing the bytes to be sent to the network
+    const signed = txn.signTxn(acct.sk);
+
+    // Send the transaction, returns the transaction id for 
+    // the first transaction in the group
+    const { txId } = await client.sendRawTransaction(signed).do();
+    txids.push(txId);
+
+    // Wait for the transaction to be confirmed.
+    const result = await algosdk.waitForConfirmation(client, txId, 2);
+
+    // Log out the confirmed round
+    console.log("Confirmed round: " + result["confirmed-round"]);
+  } catch (error) {
+    printError(error);
+    return;
+  }
+
+  console.log("Verifying challenge work...");
+  if(await validate(challenge_id, txids)){
+    console.log("Challenge completed sucessfully!");
+  }else{
+    console.log("Something went wrong :(. Please review the error message and modify the code to try again")
+  }
+
+})();
--- js/payment_transaction.js
+++ js/payment_transaction.js
@@ -10,34 +10,35 @@ const server = "http://localhost";
 const port = 4001;
 const client = new algosdk.Algodv2(token, server, port);
 
-const sk = ""; // Add your secret key
+const sk = ""; // TODO: Add your secret key
+
+// Decode the secretKey into a Uint8Array from base 64
+// This will produce an array of length 64
+const secret = new Uint8Array(Buffer.from(secretKey, "base64"));
+const acct = {
+  // The public key is the secret[32:], or the last 32 bytes
+  // We encode it to the address which is easier to read and includes a checksum
+  addr: encodeAddress(secret.slice(32)),
+  // We need not do anything with the secret
+  sk: secret,
+};
+
 const txids = [](async function () {
   try {
-    // Decode the secretKey into a Uint8Array from base 64
-    // This will produce an array of length 64
-    const secret = new Uint8Array(Buffer.from(secretKey, "base64"));
-    const acct = {
-      // The public key is the secret[32:], or the last 32 bytes
-      // We encode it to the address which is easier to read and includes a checksum
-      addr: encodeAddress(secret.slice(32)),
-      // We need not do anything with the secret
-      sk: secret,
-    };
-
-
     // Get the suggested parameters from the Algod server. These include current fee levels and suggested first/last rounds.
-    // TODO: use the client initialized above to get the suggested parameters from the algod server
+    const sp = undefined // TODO: use the client initialized above to get the suggested parameters from the algod server
 
     // Create a payment transaction from you to you using the `acct` variable defined above
     const ptxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-      // TODO: Fill out the transaction parameters to construct a transaction
-      // The sender and receiver should both be set to your address
-      // The amount should be set to 1 algo (Hint: 1 algo is 1 million micro algos)
-      // Use the suggested parameters you got from the client above
+      // TODO: Fill out the transaction parameters to send yourself 1 Algo 
+      from: undefined,    // The from (sender) should be set to your address
+      to: undefined,      // The to (receiver) should be set to your address
+      amount: undefined,  // The amount should be set to 1 algo (Hint: 1 algo is 1 million micro algos)
+      suggestedParams: sp // Use the suggested parameters you got from the client above
     });
 
     // Sign the transaction. This should return a Uint8Array representing the bytes to be sent to the network
-    const signed = _; // TODO: Sign the transaction object using the accounts secret key
+    const signed = undefined; // TODO: Sign the transaction object using the accounts secret key
 
     // Send the transaction, returns the transaction id for the first transaction in the group
     const { txId } = await client.sendRawTransaction(signed).do();
@@ -48,6 +49,7 @@ const txids = [](async function () {
 
     // Log out the confirmed round
     console.log("Confirmed round: " + result["confirmed-round"]);
+
   } catch (error) {
     printError(error);
   }
@@ -59,4 +61,5 @@ const txids = [](async function () {
       "Something went wrong, please review the error and try again"
     );
   }
+
 })();
