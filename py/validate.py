663c8e8fcc86525d65dff364865e45dbadab53fb
Cem Önel <cemonel97@gmail.com>
2022-06-28 23:04:39 +0300

    Update validate.py

--- py/validate.py
+++ py/validate.py
@@ -15,5 +15,5 @@ def validate(challenge_id, txids):
     elif result.status_code > 400 and result.status_code < 500:
         print(result.json())
         return False
-    print("Unexpected response from validator endpoint, please try again.)
+    print("Unexpected response from validator endpoint, please try again.")
     return False
